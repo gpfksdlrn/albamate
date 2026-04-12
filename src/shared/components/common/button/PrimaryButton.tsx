d106877ec7f13a8ae1cb08d8e32313a60bef8407
hizdev <gpfksdlrn@naver.com>
2026-04-12 20:30:38 +0900

    💄 Style: 비활성화 버튼 텍스트 색상 변경으로 가독성 개선

--- src/shared/components/common/button/PrimaryButton.tsx
+++ src/shared/components/common/button/PrimaryButton.tsx
@@ -80,7 +80,7 @@ const PrimaryButton = ({
 
   // variant별 스타일 정의
   const solidStyles =
-    'bg-mint-300 dark:bg-mint-350 hover:bg-mint-400 text-gray-25 disabled:bg-gray-100 disabled:hover:bg-gray-100 dark:disabled:bg-gray-100';
+    'bg-mint-300 dark:bg-mint-350 hover:bg-mint-400 text-gray-25 disabled:bg-gray-100 disabled:text-gray-400 disabled:hover:bg-gray-100 dark:disabled:bg-gray-100';
   const outlineStyles =
     'border border-mint-300 dark:border-mint-350 hover:border-mint-400 text-mint-300 hover:text-mint-400 bg-transparent disabled:border-gray-100 disabled:hover:border-gray-100 disabled:text-gray-100 disabled:hover:text-gray-100 dark:disabled:bg-gray-100';
   const cancelSolidStyles = 'bg-gray-100 text-gray-25';
