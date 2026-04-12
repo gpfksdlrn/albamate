00bc1031802ac180b8318f9fb4d37a0a5e22537c
hizdev <gpfksdlrn@naver.com>
2026-04-12 20:12:12 +0900

    🔧 Chore: 카카오 간편 로그인 UI 임시 주석처리 (미구현)

--- src/app/(auth)/account-info/page.tsx
+++ src/app/(auth)/account-info/page.tsx
@@ -1,12 +1,12 @@
 import AuthForm from '@/features/auth/components/AuthForm';
-import AuthSns from '@/features/auth/components/AuthSns';
+// import AuthSns from '@/features/auth/components/AuthSns';
 
 const AccountPage = () => {
   return (
     <>
       <h1 className="sr-only">정보 입력 페이지</h1>
       <AuthForm />
-      <AuthSns />
+      {/* <AuthSns /> */}
     </>
   );
 };
